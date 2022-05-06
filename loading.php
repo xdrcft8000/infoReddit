c21080ad4f3cf64aecf3f6b94f5af93ecd6ade34
Tayyeb Rafique <rafiquet@cardiff.ac.uk>
2022-05-06 15:10:19 +0100

    trump

--- loading.php
+++ loading.php
@@ -1,11 +1,11 @@
+<!DOCTYPE html>
 
-<?php
-  $searchterm = $_GET[‘searchterm’];
-  //$argument = 'python cgi-bin/testapi.py'.$searchterm;
-  ////  $command = escapeshellcmd($argument);
+
+  <!-- //$argument = 'python cgi-bin/testapi.py'.$searchterm;
+  //  $command = escapeshellcmd($argument);
   //  $output = shell_exec($command);
   //  echo $output;
   //print_r($searchterm);
-//  print_r($argument);
-?>
-<p><?php $searchterm  ?></p>
+//  print_r($argument); -->
+
+<p><?php $_GET['searchterm']; ?></p>
