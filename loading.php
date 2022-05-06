daa1f8cb554474bcd6f02fa5651628a6da52005a
Tayyeb Rafique <rafiquet@cardiff.ac.uk>
2022-05-06 14:59:27 +0100

    test

--- loading.php
+++ loading.php
@@ -2,7 +2,9 @@
 <?php
     $searchterm = $_GET[‘searchterm’]
     $argument = 'python cgi-bin/testapi.py'.$searchterm.
-    $command = escapeshellcmd($argument);
-    $output = shell_exec($command);
-    echo $output;
+  ////  $command = escapeshellcmd($argument);
+  //  $output = shell_exec($command);
+  //  echo $output;
+  echo $searchterm
+  echo $argument
 ?>
