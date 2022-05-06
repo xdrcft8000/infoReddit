336538f60f36968d900f52915dcca6e919ba40e3
Tayyeb Rafique <rafiquet@cardiff.ac.uk>
2022-05-06 15:30:12 +0100

    php

--- loading.php
+++ loading.php
@@ -3,7 +3,7 @@
 <body>
 <p>
 <?php
-    $argument = 'python cgi-bin/testapi.py ' . $_GET["searchterm"];
+    $argument = 'python cgi-bin/main.py ' . $_GET["searchterm"];
     $command = escapeshellcmd($argument);
     $output = shell_exec($command);
     echo $output;
