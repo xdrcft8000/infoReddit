34b6fbf63db997e49997efe5e63a2f393aa5da52
Tayyeb Rafique <rafiquet@cardiff.ac.uk>
2022-05-06 15:27:54 +0100

    swag

--- loading.php
+++ loading.php
@@ -3,11 +3,12 @@
 <body>
 <p>
 <?php
-    $argument = 'python cgi-bin/testapi.py ' . $_GET["searchterm"]
+    $argument = 'python cgi-bin/testapi.py ' . $_GET["searchterm"];
     //$command = escapeshellcmd('python cgi-bin/testapi.py ' . $_GET["searchterm"]);
     //$output = shell_exec($command);
     //echo $output;
-    echo $argument ?>
+    echo $argument;
+     ?>
 </p>
 </body>
 </html>
